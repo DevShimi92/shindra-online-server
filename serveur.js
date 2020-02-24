034c2f2dfc9495e3f0f283a70758662825f723de
DevShimi <hospitalier.philippe@gmail.com>
2020-02-24 15:38:43 +0100

    Basic serv for testing connection with phaser 3

---
+++ serveur.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io  =  require ( 'socket.io' ) . listen ( server ) ;  
+
+server.listen(8081, function () {
+  console.log(`Listening on ${server.address().port}`);
+	
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+
+});
+
